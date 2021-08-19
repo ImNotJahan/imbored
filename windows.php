bb7b46a46971c203f2496387998bae20544dfeab
Jahan Rashidi <56563402+ImNotJahan@users.noreply.github.com>
2021-08-18 18:55:09 -0700

    Create windows.php
    https://jahanrashidi.com/windows.php

---
+++ windows.php
@@ -0,0 +1,21 @@
+<?php
+$windows = isset($_GET["windows"]) ? $_GET["windows"] : 10;
+for($k = 0; $k < $windows; $k++)
+{
+    echo '<img width="10%" src="https://aw930cdnprdcd.azureedge.net/-/media/andersenwindows/images/help-me-choose/100-series-picture-interior.png">';
+}
+?>
+
+<html>
+    <head>
+       <link rel="stylesheet" href="/styles.css">
+       <title>Windows <?php echo $windows; ?></title> 
+    </head>
+    
+    <body>
+        <form>
+            <input type="hidden" name="windows" value="<?php echo $windows + 1; ?>">
+            <input type="submit" value="Show me Windows <?php echo $windows + 1; ?>">
+        </form>
+    </body>
+</html>
